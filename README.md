17e56c263623394900d3c40781700d38e5779226
danbjoseph <danbjoseph@gmail.com>
2020-04-16 15:23:17 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+each survey submission has multiple columns with text strings, and we want each text string of interest to be in its own 
+row. this script is hard-coded to one use but does that.
---
+++ index.js
@@ -0,0 +1,77 @@
+const fs = require('fs')
+const path = require('path')
+const async = require('async')
+const Papa = require('papaparse')
+
+async.waterfall([
+  function(cb) { // step 1 - grab the csv
+    console.log("reading file...");
+    fs.readFile('./files/data.csv', 'utf8', (err, data) => {
+      if (err) {
+        cb(err)
+      }
+      console.log("parsing csv...");
+      Papa.parse(data, {
+        header: true,
+        error: function(error) {
+          cb(error)
+        },
+        complete: function(results) {
+          cb(null, results.data)
+        }
+      });
+    })
+  },
+  function(data, cb) { // step 2 - parse each row into multiple rows
+    console.log("structuring data...");
+    function dataObject(row, category, string){
+      return {
+        question: category,
+        id: row['﻿id'],
+        gender: row["gender"],
+        zip: row["zip"],
+        string: string
+      }
+    }
+    var results = []
+    async.each(data, function(row, callback) { 
+      if(!!row["personal"]) {
+        results = results.concat(dataObject(row,"personal",row["personal"])) }
+      if(!!row["immediate1"]) {
+        results = results.concat(dataObject(row,"immediate",row["immediate1"])) }
+      if(!!row["immediate2"]) {
+        results = results.concat(dataObject(row,"immediate",row["immediate2"])) }
+      if(!!row["immediate3"]) {
+        results = results.concat(dataObject(row,"immediate",row["immediate3"])) }
+      if(!!row["future1"]) {
+        results = results.concat(dataObject(row,"future",row["future1"])) }
+      if(!!row["future2"]) {
+        results = results.concat(dataObject(row,"future",row["future2"])) }
+      if(!!row["future3"]) {
+        results = results.concat(dataObject(row,"future",row["future3"])) }
+      if(!!row["humanitarian_assistance"]) {
+        results = results.concat(dataObject(row,"humanitarian_assistance",row["humanitarian_assistance"])) }
+      if(!!row["comments"]) {
+        results = results.concat(dataObject(row,"comments",row["comments"])) }
+      callback();
+    }, function(err){ 
+      cb(err, results)
+    });
+  },
+  function(result, cb) { // step 3 - write the csv
+    var csv = Papa.unparse(result);
+    var fileName = "output.csv";
+    const outputFile = path.join(__dirname,'files',fileName);
+    if (fs.existsSync(outputFile)) {
+      fs.unlinkSync(outputFile);
+    }
+    fs.writeFileSync(outputFile, csv);
+  }],
+  function(err, result){
+    if(err) {
+      console.log("error:",err)
+    } else {
+      console.log("done.")
+    }
+  }
+)
